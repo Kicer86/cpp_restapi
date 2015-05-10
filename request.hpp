a1c6cce98663b1fd94df257525b6638c4a104810
Michał Walenciak <Kicer86@gmail.com>
2015-05-10 14:07:37 +0200

    github_api: introducing Request class

---
+++ request.hpp
@@ -0,0 +1,39 @@
+/*
+ * GitHub request
+ * Copyright (C) 2015  Michał Walenciak <MichalWalenciak@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef REQUEST_HPP
+#define REQUEST_HPP
+
+#include <QObject>
+
+class Request: public QObject
+{
+        Q_OBJECT
+
+    public:
+        Request();
+        Request(const Request &) = delete;
+        ~Request();
+
+        Request& operator=(const Request &) = delete;
+
+        
+};
+
+#endif // REQUEST_H
