abfab551edfa1566940eca931f2edff92c70a04a
Michał Walenciak <Kicer86@gmail.com>
2020-11-13 20:52:56 +0100

    support for token

--- src/curl_backend/connection.cpp
+++ src/curl_backend/connection.cpp
@@ -24,6 +24,9 @@ std::string GitHub::CurlBackend::Connection::get(const std::string& request)
 
     if (curl)
     {
+        curl_slist *list = nullptr;
+        std::string authorization;
+
         typedef size_t (*WriteCallback)(char *ptr, size_t size, size_t nmemb, void *userdata);
         WriteCallback write_callback = [](char *ptr, size_t size, size_t nmemb, void* result_raw)
         {
@@ -42,8 +45,17 @@ std::string GitHub::CurlBackend::Connection::get(const std::string& request)
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
         curl_easy_setopt(curl, CURLOPT_USERAGENT, "github_api/1.0");
 
+        if (m_token.empty() == false)
+        {
+            authorization = std::string("Authorization: token ") + m_token;
+            list = curl_slist_append(list, authorization.c_str());
+
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
+        }
+
         curl_easy_perform(curl);
 
+        curl_slist_free_all(list);
         curl_easy_cleanup(curl);
     }
 
