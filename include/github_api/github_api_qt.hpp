7eaefb02f01bca2a57f1bcb4465b8af2e26db072
Michał Walenciak <Kicer86@gmail.com>
2023-11-26 18:02:54 +0100

    Remove deprecated items

--- include/github_api/github_api_qt.hpp
+++
@@ -1,5 +0,0 @@
-
-#warning This file is deprecated. Please include <cpp_restapi/github/github_api_qt.hpp> instead
-#include <cpp_restapi/github/github_api_qt.hpp>
-
-namespace GitHub = cpp_restapi::GitHub;
--- tests/api_tests.cpp
+++ tests/api_tests.cpp
@@ -6,8 +6,6 @@
 
 #include "cpp_restapi/curl_connection.hpp"
 #include "cpp_restapi/qt_connection.hpp"
-#include "cpp_restapi/github/github_api_curl.hpp"
-#include "cpp_restapi/github/github_api_qt.hpp"
 #include "cpp_restapi/github/connection_builder.hpp"
 #include "cpp_restapi/github/request.hpp"
 
@@ -25,32 +23,16 @@ namespace
     constexpr int port = 9010;
 
     template<typename T>
-    T buildApi();
-
-    template<typename T>
     std::shared_ptr<IConnection> buildNewApi();
 
     template<>
-    GitHub::CurlBackend::Api buildApi<GitHub::CurlBackend::Api>()
-    {
-        return GitHub::CurlBackend::Api(std::string("http://localhost:") + std::to_string(port));
-    }
-
-    template<>
-    GitHub::QtBackend::Api buildApi<GitHub::QtBackend::Api>()
-    {
-        static QNetworkAccessManager networkmanager;
-        return GitHub::QtBackend::Api(networkmanager, QString("http://localhost:%1").arg(port));
-    }
-
-    template<>
-    std::shared_ptr<IConnection> buildNewApi<GitHub::CurlBackend::Api>()
+    std::shared_ptr<IConnection> buildNewApi<CurlBackend::Connection>()
     {
         return GitHub::ConnectionBuilder().setAddress(std::string("http://localhost:") + std::to_string(port)).build<CurlBackend::Connection>();
     }
 
     template<>
-    std::shared_ptr<IConnection>buildNewApi<GitHub::QtBackend::Api>()
+    std::shared_ptr<IConnection>buildNewApi<QtBackend::Connection>()
     {
         static QNetworkAccessManager networkmanager;
         return GitHub::ConnectionBuilder().setAddress(std::string("http://localhost:") + std::to_string(port)).build<QtBackend::Connection>(networkmanager);
@@ -71,22 +53,8 @@ class ApiTest: public testing::Test
         NiceMock<GithubServerMock> server;
 };
 
-template<typename>
-struct BackendTraits;
 
-template<>
-struct BackendTraits<GitHub::CurlBackend::Api>
-{
-    using Connection = CurlBackend::Connection;
-};
-
-template<>
-struct BackendTraits<GitHub::QtBackend::Api>
-{
-    using Connection = QtBackend::Connection;
-};
-
-using Backends = testing::Types<GitHub::CurlBackend::Api, GitHub::QtBackend::Api>;
+using Backends = testing::Types<CurlBackend::Connection, QtBackend::Connection>;
 TYPED_TEST_SUITE(ApiTest, Backends);
 
 
@@ -95,8 +63,7 @@ TYPED_TEST(ApiTest, fetchRegularUser)
     GithubServerMock::Response response(200, R"({"login":"userName1234","id":1234"})");
     ON_CALL(this->server, responseHandler).WillByDefault(Return(response));
 
-    auto api = buildApi<TypeParam>();
-    auto connection = api.connect();
+    auto connection = buildNewApi<TypeParam>();
 
     GitHub::Request request(std::move(connection));
     const auto info = request.getUserInfo("userName1234");
@@ -105,32 +72,16 @@ TYPED_TEST(ApiTest, fetchRegularUser)
 }
 
 
-TYPED_TEST(ApiTest, newInterface)
-{
-    GithubServerMock::Response response(200, R"({"login":"userName1234","id":1234"})");
-    ON_CALL(this->server, responseHandler).WillByDefault(Return(response));
-
-    using Connection = typename BackendTraits<TypeParam>::Connection;
-    auto connection = buildNewApi<TypeParam>();
-
-    GitHub::Request request(connection);
-    const auto info = request.getUserInfo("userName1234");
-
-    EXPECT_EQ(info, "{\"id\":1234,\"login\":\"userName1234\"}\n");
-}
-
-
 TYPED_TEST(ApiTest, pagination)
 {
-    auto api = buildApi<TypeParam>();
-    auto connection = api.connect();
+    auto connection = buildNewApi<TypeParam>();
 
     GithubServerMock::Response response1(200, R"({"login":"userName1234","id":1234})");
-    const std::string secondPage = api.address() + "/url/to/second/page&page=2";
+    const std::string secondPage = connection->url() + "/url/to/second/page&page=2";
     response1.addHeader( {"link", "<" + secondPage + ">; rel=\"next\""} );
 
     GithubServerMock::Response response2(200, R"({"someotherfield":"value"})");
-    const std::string thirdPage = api.address() + "/url/to/last/page&page=3";
+    const std::string thirdPage = connection->url() + "/url/to/last/page&page=3";
     response2.addHeader( {"Link", "<some_previous_page>; rel=\"prev\", <" + thirdPage + ">; rel=\"next\""} );
 
     GithubServerMock::Response response3(200, R"({"more_fields":"value234"})");
@@ -148,15 +99,14 @@ TYPED_TEST(ApiTest, pagination)
 
 TYPED_TEST(ApiTest, arraysPagination)
 {
-    auto api = buildApi<TypeParam>();
-    auto connection = api.connect();
+    auto connection = buildNewApi<TypeParam>();
 
     GithubServerMock::Response response1(200, R"([{"login":"userName1234","id":1234}])");
-    const std::string secondPage = api.address() + "/url/to/second/page&page=2";
+    const std::string secondPage = connection->url() + "/url/to/second/page&page=2";
     response1.addHeader( {"Link", "<" + secondPage + ">; rel=\"next\""} );
 
     GithubServerMock::Response response2(200, R"([{"someotherfield":"value"}])");
-    const std::string thirdPage = api.address() + "/url/to/last/page&page=3";
+    const std::string thirdPage = connection->url() + "/url/to/last/page&page=3";
     response2.addHeader( {"link", "<" + thirdPage + ">; rel=\"next\""} );
 
     GithubServerMock::Response response3(200, R"([{"more_fields":"value234"}])");
