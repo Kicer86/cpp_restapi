d53e8c25ee60a84d0cf5f048499e05487a4a4517
Kicer86 <Kicer86@users.noreply.github.com>
2023-12-09 18:27:43 +0000

    deploy: 98c72e03e334c7457b1fe2f2096d4f9b2ef52a30

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['baseconnection_0',['BaseConnection',['../dd/d4e/classcpp__restapi_1_1BaseConnection.html#ab80296bd148077bc7a5b90f8b7c593cd',1,'cpp_restapi::BaseConnection::BaseConnection(const std::string &amp;address, const std::string &amp;token)'],['../dd/d4e/classcpp__restapi_1_1BaseConnection.html#a4117e4db5721ecf54dae89b5af8868ba',1,'cpp_restapi::BaseConnection::BaseConnection(const std::string &amp;address, const std::map&lt; std::string, std::string &gt; &amp;headerEntries)']]],
+  ['baseconnection_0',['BaseConnection',['../dd/d4e/classcpp__restapi_1_1BaseConnection.html#a4117e4db5721ecf54dae89b5af8868ba',1,'cpp_restapi::BaseConnection']]],
   ['build_1',['build',['../d3/dd8/classcpp__restapi_1_1GitHub_1_1ConnectionBuilder.html#aadd92ba163b31243a4107aadbab854cc',1,'cpp_restapi::GitHub::ConnectionBuilder']]]
 ];
