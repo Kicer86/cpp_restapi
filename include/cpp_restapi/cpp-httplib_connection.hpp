2ef61137056765e86c9d45090729e07d722780f9
Michał Walenciak <Kicer86@gmail.com>
2023-12-09 13:39:26 +0100

    Prepare base for new backend

---
+++ include/cpp_restapi/cpp-httplib_connection.hpp
@@ -0,0 +1,22 @@
+
+
+#include "base_connection.hpp"
+#include "cpp_restapi_export.h"
+
+namespace cpp_restapi::CppHttplibBackend {
+
+    class CPP_RESTAPI_EXPORT Connection: public BaseConnection
+    {
+        public:
+            Connection(const std::string& address, const std::map<std::string, std::string>& headerEntries);
+            Connection(const Connection &) = delete;
+
+            ~Connection();
+
+            Connection& operator=(const Connection &) = delete;
+
+            std::pair<std::string, std::string> fetchPage(const std::string& request) override;
+    };
+
+}
+
