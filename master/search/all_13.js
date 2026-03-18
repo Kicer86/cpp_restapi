8e835f3aafc8a88f0e17636915393066bded2afa
Kicer86 <Kicer86@users.noreply.github.com>
2026-03-18 21:49:41 +0000

    deploy: d40867b856be80a3882054ca071e294d28dc6eb8

--- master/search/all_13.js
+++ master/search/all_13.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['_7econnection_0',['~Connection',['../d6/d5e/classcpp__restapi_1_1CurlBackend_1_1Connection.html#a9e13a03e1df26808382a1cdce93a4907',1,'cpp_restapi::CurlBackend::Connection::~Connection()'],['../d2/dbb/classcpp__restapi_1_1QtBackend_1_1Connection.html#aa2873efd95efc14d1e93b675489aa288',1,'cpp_restapi::QtBackend::Connection::~Connection()']]],
+  ['_7econnection_0',['~connection',['../d6/d5e/classcpp__restapi_1_1CurlBackend_1_1Connection.html#a9e13a03e1df26808382a1cdce93a4907',1,'cpp_restapi::CurlBackend::Connection::~Connection()'],['../d2/dbb/classcpp__restapi_1_1QtBackend_1_1Connection.html#aa2873efd95efc14d1e93b675489aa288',1,'cpp_restapi::QtBackend::Connection::~Connection()']]],
   ['_7eiconnection_1',['~IConnection',['../de/dd0/structcpp__restapi_1_1IConnection.html#ab85ba8ffe44f1ebfa2ff329c007cc4f0',1,'cpp_restapi::IConnection']]],
   ['_7eipaginationstrategy_2',['~IPaginationStrategy',['../db/d62/structcpp__restapi_1_1IPaginationStrategy.html#ad33c28df361568604b50426165071733',1,'cpp_restapi::IPaginationStrategy']]],
   ['_7eisseconnection_3',['~ISseConnection',['../d6/dff/structcpp__restapi_1_1ISseConnection.html#a01501c23ae609b14054ab1bad3ceb456',1,'cpp_restapi::ISseConnection']]],
