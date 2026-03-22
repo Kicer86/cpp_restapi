a4d5a5d2882e8792873c2807e46e31adb379199b
Michał Walenciak <michalwalenciak@gmail.com>
2026-03-22 10:48:10 +0100

    Introduce per-backend factory functions

---
+++ src/curl_backend/create_connection.cpp
@@ -0,0 +1,20 @@
+
+#include <cpp_restapi/create_curl_connection.hpp>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#include <cpp_restapi/curl_connection.hpp>
+#pragma GCC diagnostic pop
+
+namespace cpp_restapi
+{
+    std::unique_ptr<IConnection> createCurlConnection(
+        const std::string& address,
+        const std::map<std::string, std::string>& headerEntries)
+    {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+        return std::make_unique<CurlBackend::Connection>(address, headerEntries);
+#pragma GCC diagnostic pop
+    }
+}
