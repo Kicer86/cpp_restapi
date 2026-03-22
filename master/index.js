861853d424b76dcd66deae052b5064c73a75532b
Kicer86 <Kicer86@users.noreply.github.com>
2026-03-22 21:11:54 +0000

    deploy: 0dc1492f4b92fdb5ccd46817d0a055338284f5db

--- master/index.js
+++ master/index.js
@@ -1,33 +1,33 @@
 var index =
 [
-    [ "Submodules", "index.html#autotoc_md1", [
-      [ "How to use it", "index.html#autotoc_md2", null ],
-      [ "Examples", "index.html#autotoc_md5", null ],
-      [ "Simplest usage", "index.html#autotoc_md6", [
-        [ "Dedicated GitHub helpers", "index.html#autotoc_md7", [
-          [ "Qt example", "index.html#autotoc_md8", null ],
-          [ "libcurl example", "index.html#autotoc_md9", null ],
-          [ "cpp-httplib example:", "index.html#autotoc_md10", null ]
-        ] ]
-      ] ],
-      [ "Server-Sent Events (SSE)", "index.html#autotoc_md11", [
-        [ "SSE with curl", "index.html#autotoc_md12", null ],
-        [ "SSE with Qt", "index.html#autotoc_md13", null ],
-        [ "SSE with cpp-httplib", "index.html#autotoc_md14", null ],
-        [ "SseEvent fields", "index.html#autotoc_md15", null ]
-      ] ],
-      [ "Asynchronous Requests", "index.html#autotoc_md16", [
-        [ "Async with curl", "index.html#autotoc_md17", null ],
-        [ "Async with Qt", "index.html#autotoc_md18", null ],
-        [ "Cancellation", "index.html#autotoc_md19", null ],
-        [ "Async pagination", "index.html#autotoc_md20", null ]
-      ] ],
-      [ "C++20 Coroutine Helpers", "index.html#autotoc_md21", [
-        [ "Key types", "index.html#autotoc_md22", null ],
-        [ "Example (curl backend)", "index.html#autotoc_md23", null ]
-      ] ],
-      [ "Building examples", "index.html#autotoc_md24", null ],
-      [ "Building unit tests", "index.html#autotoc_md25", null ],
-      [ "Links", "index.html#autotoc_md26", null ]
-    ] ]
+    [ "How to use it", "index.html#autotoc_md2", [
+      [ "Submodules", "index.html#autotoc_md1", null ]
+    ] ],
+    [ "Examples", "index.html#autotoc_md5", null ],
+    [ "Simplest usage", "index.html#autotoc_md6", [
+      [ "Dedicated GitHub helpers", "index.html#autotoc_md7", [
+        [ "Qt example", "index.html#autotoc_md8", null ],
+        [ "libcurl example", "index.html#autotoc_md9", null ],
+        [ "cpp-httplib example:", "index.html#autotoc_md10", null ]
+      ] ]
+    ] ],
+    [ "Server-Sent Events (SSE)", "index.html#autotoc_md11", [
+      [ "SSE with curl", "index.html#autotoc_md12", null ],
+      [ "SSE with Qt", "index.html#autotoc_md13", null ],
+      [ "SSE with cpp-httplib", "index.html#autotoc_md14", null ],
+      [ "SseEvent fields", "index.html#autotoc_md15", null ]
+    ] ],
+    [ "Asynchronous Requests", "index.html#autotoc_md16", [
+      [ "Async with curl", "index.html#autotoc_md17", null ],
+      [ "Async with Qt", "index.html#autotoc_md18", null ],
+      [ "Cancellation", "index.html#autotoc_md19", null ],
+      [ "Async pagination", "index.html#autotoc_md20", null ]
+    ] ],
+    [ "C++20 Coroutine Helpers", "index.html#autotoc_md21", [
+      [ "Key types", "index.html#autotoc_md22", null ],
+      [ "Example (curl backend)", "index.html#autotoc_md23", null ]
+    ] ],
+    [ "Building examples", "index.html#autotoc_md24", null ],
+    [ "Building unit tests", "index.html#autotoc_md25", null ],
+    [ "Links", "index.html#autotoc_md26", null ]
 ];
